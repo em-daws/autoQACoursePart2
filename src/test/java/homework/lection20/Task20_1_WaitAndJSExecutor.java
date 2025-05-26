3921e03100a7484a396677229633da44b555272e
Tetiana Babeshko <amy11dawson@gmail.com>
2025-05-26 22:57:25 +0300

    resolved HW 20.1

--- src/test/java/homework/lection20/Task20_1_WaitAndJSExecutor.java
+++ src/test/java/homework/lection20/Task20_1_WaitAndJSExecutor.java
@@ -7,7 +7,6 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.FluentWait;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
-import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
